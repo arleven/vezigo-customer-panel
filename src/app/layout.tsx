6f31cb882705a5899ff6010c5528018776f1f5e5
Mayank Soni <mayanksoni282@gmail.com>
2024-08-06 15:38:05 +0530

    update: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,38 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 
-import { ThemeProvider } from "@/components/theme-provider";
-import type { Metadata } from "next";
-import { CartProvider } from "@/context/cart-context";
+import { ThemeProvider } from '@/components/theme-provider';
+import type { Metadata } from 'next';
+import { CartProvider } from '@/context/cart-context';
 
-import { Poppins } from "next/font/google";
+import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-	subsets: ["latin"],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-	title: "Shop cart",
-	description: "Shopping cart",
+	title: 'Vezigo',
+	description: 'Discover the best and fresh vegetables for all your needs'
 };
 
 export default function RootLayout({
-	children,
+	children
 }: {
 	children: React.ReactNode;
 }) {
 	return (
 		<CartProvider>
-			<html lang="en">
-				<head><link rel="icon" href="/favicon.ico" sizes="any" /></head>
+			<html lang='en'>
+				<head>
+					<link rel='icon' href='/favicon.ico' sizes='any' />
+				</head>
 				<body className={poppins.className}>
-					<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='system'
+						enableSystem
+					>
 						{children}
 					</ThemeProvider>
 				</body>
