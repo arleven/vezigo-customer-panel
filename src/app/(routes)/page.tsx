6b1708ec76b2f0b9af51c8210a9282e5c506448a
Mayank Soni <mayanksoni282@gmail.com>
2024-08-09 12:31:15 +0530

    update: padding and gap

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -60,7 +60,7 @@ export default async function HomePage() {
 							Featured Vegetables
 						</h2>
 					</div>
-					<div className='grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+					<div className='grid grid-cols-2 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 						{vegetables.map((vegetable) => (
 							<ProductCard
 								key={vegetable.id}
@@ -84,7 +84,7 @@ export default async function HomePage() {
 							Featured Fruits
 						</h2>
 					</div>
-					<div className='grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+					<div className='grid grid-cols-2 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 						{fruits.map((fruit) => (
 							<ProductCard
 								key={fruit.id}
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -113,7 +113,7 @@ export function Products({ products, pageCount }: ProductsProps) {
 						</p>
 					</div>
 				) : null}
-				<div className='grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+				<div className='grid grid-cols-2 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 					{products.map((product) => (
 						<ProductCard key={product.id} product={product} />
 					))}
