b66b3618cb737e64d59e16c18a2255d9da84da6f
Mayank Soni <mayanksoni282@gmail.com>
2024-09-20 16:39:50 +0530

    refactor: Improve UI layout in ProductCard component.

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,32 +1,25 @@
 'use client';
 
-import { cn, formatPrice } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 import type { Package, Product } from '@/types';
 
 import * as React from 'react';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
-import { AspectRatio } from './ui/aspect-ratio';
-import { Icons } from './icons';
-import { Button, buttonVariants } from './ui/button';
+import { Button } from './ui/button';
 import {
 	Card,
 	CardContent,
 	CardDescription,
 	CardFooter,
-	CardHeader,
 	CardTitle
 } from './ui/card';
 import { useCart } from '@/context/cart-context';
-import { Input } from './ui/input';
-import { CartItemActions } from './cart/update-cart';
-import { map } from 'zod';
 import {
 	Dialog,
 	DialogClose,
 	DialogContent,
 	DialogDescription,
-	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger
@@ -50,10 +43,7 @@ export function ProductCard({
 	showCartButton = true,
 	...props
 }: ProductCardProps) {
-	const { addToCart, cartItems, updateCartItemQuantity, removeFromCart } =
-		useCart();
-
-	const [quantity, setQuantity] = React.useState(0);
+	const { addToCart, cartItems } = useCart();
 
 	const handleAddToCart = (pack: Package, product: Product) => {
 		addToCart(pack, product);
@@ -87,68 +77,27 @@ export function ProductCard({
 				className='object-cover h-48 w-96 rounded-2xl'
 				loading='lazy'
 			/>
-			<Badge className='absolute right-4 bottom-24 justify-center hover:bg-red-600 bg-red-600 text-white font-bold text-base rounded-2xl p-2'>
+			<Badge className='absolute right-4 top-[150px] justify-center hover:bg-red-600 bg-red-600 text-white font-bold text-base rounded-2xl p-2'>
 				{`₹${product?.marketPrice?.toUpperCase()}`}
 			</Badge>
 			<CardContent className='grid gap-2.5 p-2'>
 				<CardTitle className='text-base'>
 					{product.title}
-					<CardDescription className='line-clamp-2'>
-						{product?.description}
-					</CardDescription>
+					{product?.description && (
+						<CardDescription className='line-clamp-2'>
+							{product?.description}
+						</CardDescription>
+					)}
 				</CardTitle>
 			</CardContent>
 			{showCartButton && (
 				<CardFooter className='p-1'>
 					<div className='flex w-full flex-col items-center gap-2 sm:flex-row sm:justify-between'>
-						{/* {isProductInCart ? (
-							<div className='w-full'>
-								<div className='flex items-center space-x-1'>
-									<Input
-										className='h-8 w-full text-base rounded-xl'
-										type='number'
-										min='1'
-										value={quantity}
-										onChange={(e) => {
-											setQuantity(Number(e.target.value));
-											updateCartItemQuantity(
-												product.id,
-												Number(e.target.value)
-											);
-										}}
-									/>
-
-									<Button
-										variant='outline'
-										size='icon'
-										className='h-8 w-8 bg-red-500 hover:bg-red-400 text-white hover:text-white rounded-lg'
-										onClick={() => {
-											removeFromCart(product.id);
-										}}
-									>
-										<Icons.trash className='h-4 w-4' />
-									</Button>
-								</div>
-							</div>
-						) : (
-							<Button
-								size='sm'
-								className='h-8 w-full rounded-xl bg-green-500 hover:bg-green-600'
-								onClick={() => {
-									handleAddToCart(product);
-								}}
-							>
-								Add to cart
-							</Button>
-						)} */}
 						<Dialog>
 							<DialogTrigger asChild>
 								<Button
 									size='sm'
 									className='h-8 w-full rounded-xl bg-green-500 hover:bg-green-600'
-									/* onClick={() => {
-										handleAddToCart(product);
-									}} */
 								>
 									Add to cart
 								</Button>
@@ -174,10 +123,6 @@ export function ProductCard({
 											<ToggleGroupItem
 												value={String(pack.quantity)}
 												onClick={() => {
-													/* updateCartItemQuantity(
-														product.id,
-														Number(pack.quantity)
-													); */
 													handleAddToCart(
 														pack,
 														product
