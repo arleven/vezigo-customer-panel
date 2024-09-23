814d59dd96ec3bfcdaabd26daf9809a047fb287d
Mayank Soni <mayanksoni282@gmail.com>
2024-09-23 12:00:43 +0530

    refactor: Update WhatsApp API URL to use 'wa.me' domain.

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -7,7 +7,7 @@ export const apiUrl: string = process.env.NEXT_PUBLIC_API_URL as string;
 export const links = {
 	rickroll: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 	// whatsAppApiUrl: 'whatsapp://send',
-	whatsAppApiUrl: 'https://api.whatsapp.com/send',
+	whatsAppApiUrl: 'https://wa.me/send',
 	siteAddress: process.env.NEXT_PUBLIC_SITE_ADDRESS as string
 };
 
