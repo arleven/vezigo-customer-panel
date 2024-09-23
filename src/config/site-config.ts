823af3e8547116b3d78b3ae5a4cdbfca15032929
Mayank Soni <mayanksoni282@gmail.com>
2024-09-23 16:07:07 +0530

    fix: update WhatsApp API URL for Safari compatibility.

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -6,9 +6,9 @@ export const apiUrl: string = process.env.NEXT_PUBLIC_API_URL as string;
 
 export const links = {
 	rickroll: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-	// whatsAppApiUrl: 'whatsapp://send',
 	regularWhatsAppApiUrl: 'https://wa.me',
-	safariWhatsAppApiUrl: 'https://api.whatsapp.com/send',
+	safariWhatsAppApiUrl: 'whatsapp://send',
+	// safariWhatsAppApiUrl: 'https://api.whatsapp.com/send',
 	siteAddress: process.env.NEXT_PUBLIC_SITE_ADDRESS as string
 };
 
