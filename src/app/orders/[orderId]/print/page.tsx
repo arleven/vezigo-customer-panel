d570bbcc1a88303797fb8a2000ab91d421c727ef
Mayank Soni <mayanksoni282@gmail.com>
2024-11-19 17:50:04 +0530

    update: order print page layout

--- src/app/orders/[orderId]/print/page.tsx
+++ src/app/orders/[orderId]/print/page.tsx
@@ -11,9 +11,10 @@ import {
 import { Order } from '@/types';
 import { formatPrice } from '@/lib/utils';
 import { apiUrl } from '@/config/site-config';
-import { Shell } from '@/components/Shells/shell';
-import { Separator } from '@/components/ui/separator';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+
+import sabjiKingLogo from '@/assets/sabjiking.png';
+import sabjiKingQr from '@/assets/sabji-king-web-qr.png';
 
 async function getOrder(orderId: string): Promise<Order> {
 	try {
@@ -21,6 +22,7 @@ async function getOrder(orderId: string): Promise<Order> {
 		const response = await axios.get(orderApiUrl);
 		const responseData = await response.data;
 		const { data } = responseData;
+
 		return data;
 	} catch (error) {
 		console.error('Error fetching product data:', error);
@@ -41,48 +43,76 @@ export default async function OrderPage({ params }: OrderPageProps) {
 	const deliveryCost = formatPrice(Number(40), 'INR');
 
 	const orderDate = new Date(order.createdAt).toLocaleString([], {
-		dateStyle: 'medium',
-		timeStyle: 'medium'
+		dateStyle: 'medium'
+		// timeStyle: 'medium'
 	});
 
 	const billAmount = formatPrice(Number(order.billAmount), 'INR');
 
 	return (
 		<html lang='en'>
-			<body className='flex flex-col relative min-h-screen bg-gray-100'>
-				<Shell as='div' className='gap-12'>
+			<body
+				className='flex flex-col relative min-h-screen'
+				style={{ backgroundColor: '#ffde08' }}
+			>
+				<div className='gap-12 p-4'>
 					<main className='flex flex-1 flex-col gap-4 md:gap-8'>
-						<div className='flex items-center gap-4'>
-							<h1 className='font-semibold text-lg md:text-xl'>
-								<span className='font-normal text-gray-500 dark:text-gray-400'>
-									Order ID: <strong>{order.orderId}</strong>
-									<br />
-									Ordered by {order.userDetails.name}
-								</span>
-								<span className='font-normal text-gray-500 dark:text-gray-400'>
-									{' '}
-									on {orderDate}
-								</span>
-							</h1>
+						<div className='flex items-center'>
+							<div>
+								<Image
+									src={sabjiKingLogo}
+									width={200}
+									height={200}
+									className='rounded'
+									alt='Sabji King Logo'
+								/>
+							</div>
+							<div className='ml-auto'>
+								<Image
+									src={sabjiKingQr}
+									width={70}
+									height={70}
+									className='rounded ml-auto'
+									alt='Sabji King QR'
+								/>
+								<h4 className='text-right'>
+									Scan the code above order again.{' '}
+								</h4>
+							</div>
 						</div>
-						<div className='flex flex-col md:grid md:grid-cols-6 gap-6'>
-							<div className='md:col-span-4 lg:col-span-3 xl:col-span-4 flex flex-col gap-6'>
+
+						<div className='flex flex-col gap-2'>
+							<div className='flex flex-col gap-2'>
 								<Card>
 									<CardHeader>
-										<CardTitle>Ordered Items</CardTitle>
+										<CardTitle className='text-center text-lg -m-4'>
+											Retail Invoice
+										</CardTitle>
 									</CardHeader>
 									<CardContent>
-										<Table>
+										<div className='flex items-center gap-4'>
+											<div className='font-semibold text-base'>
+												Order ID: {order.orderId}
+											</div>
+											<div className='font-semibold text-base ml-auto'>
+												Ordered at: {orderDate}
+											</div>
+										</div>
+										<Table className='mt-4'>
 											<TableHeader>
-												<TableRow>
-													<TableHead className='hidden md:table-cell' />
-													<TableHead className='max-w-[150px]'>
-														Item Name
+												<TableRow className='hover:bg-inherit'>
+													<TableHead className='max-w-[150px] px-0 text-sm'>
+														Particulars
 													</TableHead>
-													<TableHead>
+													<TableHead className='text-right text-sm'>
 														Quantity
 													</TableHead>
-													<TableHead>Total</TableHead>
+													<TableHead className='text-right text-sm'>
+														Rate
+													</TableHead>
+													<TableHead className='text-right text-sm'>
+														Amount
+													</TableHead>
 												</TableRow>
 											</TableHeader>
 											<TableBody>
@@ -91,36 +121,32 @@ export default async function OrderPage({ params }: OrderPageProps) {
 														item: any,
 														index: number
 													) => (
-														<TableRow key={index}>
-															<TableCell className='hidden md:table-cell'>
-																<Image
-																	src={
-																		item
-																			.product
-																			.imageUrl
-																	}
-																	alt={
-																		item
-																			.product
-																			.title
-																	}
-																	className='aspect-square rounded-md object-cover'
-																	width={64}
-																	height={64}
-																/>
-															</TableCell>
-															<TableCell className='font-medium'>
+														<TableRow
+															className='hover:bg-inherit'
+															key={index}
+														>
+															<TableCell className='py-1 text-sm p-0'>
 																{
 																	item.product
 																		.title
 																}{' '}
 																{`(${item.pack.quantity}${item.pack.unit})`}
 															</TableCell>
-															<TableCell>
+															<TableCell className='py-1 text-sm text-right'>
 																{item.quantity}
 																{item.unit}
 															</TableCell>
-															<TableCell>
+															<TableCell className='py-1 text-sm text-right'>
+																{formatPrice(
+																	Number(
+																		item
+																			.pack
+																			.price
+																	),
+																	'INR'
+																)}
+															</TableCell>
+															<TableCell className='py-1 text-sm text-right'>
 																{formatPrice(
 																	Number(
 																		item
@@ -135,58 +161,48 @@ export default async function OrderPage({ params }: OrderPageProps) {
 														</TableRow>
 													)
 												)}
+												<TableRow className='hover:bg-inherit'>
+													<TableCell></TableCell>
+													<TableCell></TableCell>
+													<TableCell className='py-1 text-sm'>
+														Delivery Cost
+													</TableCell>
+													<TableCell className='py-1 text-sm text-right'>
+														{deliveryCost}
+													</TableCell>
+												</TableRow>
+												<TableRow className='hover:bg-inherit'>
+													<TableCell></TableCell>
+													<TableCell></TableCell>
+													<TableCell className='py-1 text-sm'>
+														Total
+													</TableCell>
+													<TableCell className='py-1 text-sm text-right'>
+														{billAmount}
+													</TableCell>
+												</TableRow>
 											</TableBody>
 										</Table>
 									</CardContent>
+									<div className='md:col-span-2 lg:col-span-3 xl:col-span-2 flex flex-col gap-6'>
+										<CustomerDetailsCard order={order} />
+									</div>
 								</Card>
-								<PaymentCard
-									billAmount={billAmount}
-									deliveryCost={deliveryCost}
-								/>
-							</div>
-							<div className='md:col-span-2 lg:col-span-3 xl:col-span-2 flex flex-col gap-6'>
-								<CustomerDetailsCard order={order} />
 							</div>
 						</div>
 					</main>
-				</Shell>
+				</div>
 			</body>
 		</html>
 	);
 }
 
-const PaymentCard = (props: { billAmount: string; deliveryCost: string }) => {
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Payment</CardTitle>
-			</CardHeader>
-			<CardContent className='grid gap-4'>
-				<div className='flex items-center'>
-					<div>Mode</div>
-					<div className='ml-auto'>Offline</div>
-				</div>
-				<Separator />
-				<div className='flex items-center'>
-					<div>Delivery Cost</div>
-					<div className='ml-auto'>{props.deliveryCost}</div>
-				</div>
-				<Separator />
-				<div className='flex items-center font-medium'>
-					<div>Total</div>
-					<div className='ml-auto'>{props.billAmount}</div>
-				</div>
-			</CardContent>
-		</Card>
-	);
-};
-
 const CustomerDetailsCard = (props: { order: Order }) => {
 	return (
-		<Card>
-			<div>
-				<CardHeader className='flex flex-row items-center space-y-0'>
-					<CardTitle>Customer</CardTitle>
+		<div className='grid gap-x-2 gap-y-2 lg:mx-0 lg:max-w-none grid-cols-3'>
+			<Card className='rounded-xl border-0'>
+				<CardHeader className='py-0 flex flex-row items-center space-y-0'>
+					<CardTitle className='text-lg'>Customer Details</CardTitle>
 				</CardHeader>
 				<CardContent className='text-sm'>
 					<div className='grid gap-1'>
@@ -194,25 +210,23 @@ const CustomerDetailsCard = (props: { order: Order }) => {
 						<div>{props.order.userDetails.phoneNumber}</div>
 					</div>
 				</CardContent>
-			</div>
-			<Separator />
-			<div>
-				<CardHeader>
-					<CardTitle>Address</CardTitle>
+			</Card>
+			<Card className='rounded-xl border-0'>
+				<CardHeader className='py-0 '>
+					<CardTitle className='text-lg'>Address</CardTitle>
 				</CardHeader>
 				<CardContent className='text-sm'>
 					<div>{props.order.userDetails.address ?? ''}</div>
 				</CardContent>
-			</div>
-			<Separator />
-			<div>
-				<CardHeader>
-					<CardTitle>Notes</CardTitle>
+			</Card>
+			<Card className='rounded-xl border-0'>
+				<CardHeader className='py-0 '>
+					<CardTitle className='text-lg'>Notes</CardTitle>
 				</CardHeader>
 				<CardContent className='text-sm'>
 					{props.order.userDetails.notes}
 				</CardContent>
-			</div>
-		</Card>
+			</Card>
+		</div>
 	);
 };
